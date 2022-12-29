733ad57e4130c482ad6501b4939345efa281280b
siminsadanandan <44857730+siminsadanandan@users.noreply.github.com>
2022-12-30 00:17:08 +0530

    Update script.js

--- xk6-browser-demo/assets/script.js
+++ xk6-browser-demo/assets/script.js
@@ -3,36 +3,24 @@ import { check, sleep } from 'k6';
 
 export default function () {
   const browser = chromium.launch({ headless: true});
-  const context = browser.newContext({screen: {width: 1920,height: 1080},viewport: {width: 1920,height: 1080},});
+  //context=browser.newContext({screen: {width: 1920,height: 1080},viewport: {width: 1920,height: 1080},});
   const page = browser.newPage();
 
-//  page.goto ('https://portal-qa.connect.cloud.precisely.services/', { waitUntil: 'networkidle' });
-//  page.waitForLoadState('networkidle');
-//  //page.waitForNavigation();
-//  page.waitForNavigation({waitUntil: 'load'});
-//  page.screenshot({ path: 'screenshot1.png' });
-//  page.waitForSelector('#idp-discovery-username'); 
-//  page.screenshot({ path: 'screenshot2.png' });
-//  page.close();
-//  browser.close();
-//
-   page
-   .goto('https://portal-qa.connect.cloud.precisely.services/', { waitUntil: 'networkidle' })
-     .then(() => {
-       page.waitForLoadState('networkidle');
-       //not working
-       //page.waitForNavigation();
-       page.waitForNavigation({waitUntil: 'load'});
-       //sleep (10);
-       //not working
-       //page.waitForLoadState({ waitUntil: 'domcontentloaded' });
-       page.screenshot({ path: 'screenshot1.png' });
-       page.waitForSelector('#idp-discovery-username',{state:'attached'}); 
-       page.$('#idp-discovery-username').type('petnt103-designer1@precisely.testinator.com');
-       page.screenshot({ path: 'screenshot2.png' });
-     })
-     .finally(() => {
-       page.close();
-       browser.close();
-     });
-}
+  page
+    .goto('https://test.k6.io/', { waitUntil: 'networkidle' })
+    .then(() => {
+      page.waitForLoadState('networkidle');
+      //not working
+      //page.waitForNavigation();
+      //page.waitForNavigation({waitUntil: 'load'});
+      //sleep (10);
+      //not working
+      //page.waitForLoadState({ waitUntil: 'domcontentloaded' });
+      //page.screenshot({ path: 'screenshot1.png' });
+      //page.waitForSelector('#idp-discovery-username'); 
+      page.screenshot({ path: 'screenshot2.png' });
+    })
+    .finally(() => {
+      page.close();
+      browser.close();
+    });
