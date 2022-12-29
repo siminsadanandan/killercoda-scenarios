afbcca7b5b9ff8a393ac9532e268b44fe5a721b5
Simin Sadanandan <simin.sadanandan@gmail.com>
2022-12-29 23:01:50 +0530

    scripts testing

--- xk6-browser-demo/assets/script.js
+++ xk6-browser-demo/assets/script.js
@@ -3,26 +3,28 @@ import { check, sleep } from 'k6';
 
 export default function () {
   const browser = chromium.launch({ headless: true});
-  //context=browser.newContext({screen: {width: 1920,height: 1080},viewport: {width: 1920,height: 1080},});
+  const context = browser.newContext({screen: {width: 1920,height: 1080},viewport: {width: 1920,height: 1080},});
   const page = browser.newPage();
 
-  page
-    .goto('https://test.k6.io/', { waitUntil: 'networkidle' })
-    .then(() => {
-      page.waitForLoadState('networkidle');
-      //not working
-      //page.waitForNavigation();
-      //page.waitForNavigation({waitUntil: 'load'});
-      //sleep (10);
-      //not working
-      //page.waitForLoadState({ waitUntil: 'domcontentloaded' });
-      //page.screenshot({ path: 'screenshot1.png' });
-      //page.waitForSelector('#idp-discovery-username'); 
-      page.screenshot({ path: 'screenshot2.png' });
-    })
-    .finally(() => {
-      page.close();
-      browser.close();
-    });
+  page.goto ('https://portal-qa.connect.cloud.precisely.services/', { waitUntil: 'networkidle' });
+  
+  // page
+  // .goto('https://test.k6.io/', { waitUntil: 'networkidle' })
+  //   .then(() => {
+  //     page.waitForLoadState('networkidle');
+  //     //not working
+  //     //page.waitForNavigation();
+  //     //page.waitForNavigation({waitUntil: 'load'});
+  //     //sleep (10);
+  //     //not working
+  //     //page.waitForLoadState({ waitUntil: 'domcontentloaded' });
+  //     //page.screenshot({ path: 'screenshot1.png' });
+  //     //page.waitForSelector('#idp-discovery-username'); 
+  //     page.screenshot({ path: 'screenshot2.png' });
+  //   })
+  //   .finally(() => {
+  //     page.close();
+  //     browser.close();
+  //   });
 }
 
