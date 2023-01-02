0d96128525ccdbeb12d21d0c2dbf916a284aa61c
Simin Sadanandan <simin.sadanandan@gmail.com>
2023-01-02 10:00:18 +0530

    edited

--- floodio-element-demo/assets/example2.perf.ts
+++ floodio-element-demo/assets/example2.perf.ts
@@ -26,7 +26,11 @@ interface UserData {
   password: string
 }
 
-//const globalBrowserID = ENV.FLOOD_GRID_INDEX + '_' + ENV.FLOOD_GRID_NODE_SEQUENCE_ID + '_' + ENV.BROWSER_ID.toString()
+const url = 'https://portal-qa.connect.cloud.precisely.services/'
+const ssoUrl = 'https://sso.precisely.services/'
+
+const globalBrowserID = ENV.FLOOD_GRID_INDEX + '_' + ENV.FLOOD_GRID_NODE_SEQUENCE_ID + '_' + ENV.BROWSER_ID.toString()
+
 //TestData.fromCSV<UserData>('users.csv').filter((line, index, browserID) => line.id === globalBrowserID)
 TestData.fromCSV<UserData>("users.csv").shuffle().circular()
 //TestData.fromCSV<UserData>("users.csv").shuffle(false).circular()
@@ -48,18 +52,14 @@ export default () => {
 	// afterEach(async browser => {})
 
 	step('Launch URL', async (browser: Browser, data: UserData) => {
-	   // visit instructs the browser to launch, open a page, and navigate to https://portal-qa.connect.cloud.precisely.services/
-	   await browser.visit('https://portal-qa.connect.cloud.precisely.services/')
-
+	   // visit instructs the browser to launch, open a page, and navigate to the url
+	   await browser.visit(url)
 	   await browser.wait(Until.elementIsEnabled(By.id('idp-discovery-username')))
-
 	   await browser.type(By.id('idp-discovery-username'), data.username)
-
-	   //console.log(data.username);
-
+	   //console.log(data.username)
 	})
 
-	step('Navigate to password page', async (browser: Browser, data: UserData) => {
+	step('Navigate to password page', async (browser: Browser) => {
 	   await browser.click(By.id('idp-discovery-submit'))
 	   await browser.wait(Until.elementIsEnabled(By.id('okta-signin-password')))
 	})
@@ -67,16 +67,13 @@ export default () => {
 	step('Login', async (browser: Browser, data: UserData) => {
 	   await browser.type(By.id('okta-signin-password'), data.password)
 	   await browser.click(By.id('okta-signin-submit'))
-	   await browser.wait(Until.urlIs("https://portal-qa.connect.cloud.precisely.services/data-integration/home/pipeline"))
-	   await browser.takeScreenshot()
+	   await browser.wait(Until.urlIs(`${url}data-integration/home/pipeline`))
 	})
 	
-	step('Logout', async (browser: Browser, data: UserData) => {
+	step('Logout', async (browser: Browser) => {
 	   await browser.click(By.attr('a','data-cy','user-account-image'))
 	   await browser.takeScreenshot()
 	   await browser.click(By.attr('a','data-cy','user-signout'))
-	   await browser.takeScreenshot()
-	   await browser.wait(Until.urlIs("https://sso.precisely.services/"))
-	   await browser.takeScreenshot()
+	   await browser.wait(Until.urlIs(ssoUrl))
 	})
 }
